b067d817b501cbb9ffe359cd014d6220c3332232
Daniel Deutsch <daniel.deutsch@dear-innovations.com>
2024-03-19 20:39:25 +0100

    finish chapter 2

--- exercises/chapter2/src/Euler.purs
+++ exercises/chapter2/src/Euler.purs
@@ -6,6 +6,6 @@ import Data.Foldable (sum)
 
 ns n = range 0 (n - 1)
 
-multiples n = filter (\n -> mod n 3 == 0 || mod n 5 == 0) (ns n)
+multiples n = filter (\x -> mod x 3 == 0 || mod x 5 == 0) (ns n)
 
 answer n = sum (multiples n)
--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,15 @@
-module Test.MySolutions where
+module MySolutions (diagonal, circleArea, leftoverCents) where
 
 import Prelude
+
+import Data.Int (rem)
+import Data.Number (pi, sqrt)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea :: Number -> Number
+circleArea r = pi * r * r
+
+leftoverCents :: Int -> Int
+leftoverCents cents = rem cents 100
